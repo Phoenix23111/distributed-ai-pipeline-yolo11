779e2daadd63369c54ec244ff733c7c1c7c4ef4b
Phoenix23111 <mustehzar2311@gmail.com>
2026-06-23 11:59:13 +0500

    modified the url

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ if uploaded_file is not None:
             print(uploaded_file.getvalue())
 
             try:
-                response = requests.post("http://127.0.0.1:8000/api/v1/process",files=files)
+                response = requests.post("https://distributed-ai-pipeline-yolo11.onrender.com/api/v1/process",files=files)
 
                 # check if Api caught it successfully
                 if response.status_code == 200:
@@ -35,7 +35,7 @@ if uploaded_file is not None:
                     status = "processing"
                     while status != "completed":
                         # Ask the API 
-                        status_response = requests.get(f"http://127.0.0.1:8000/api/v1/status/{data['task_id']}")
+                        status_response = requests.get(f"https://distributed-ai-pipeline-yolo11.onrender.com/v1/status/{data['task_id']}")
                         status_data = status_response.json()
                         # Check What API says
                         status = status_data.get("status")
